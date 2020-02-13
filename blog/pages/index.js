1db25a2e3e6c6a391d35bbabf60483869c2bafe6
Jacky-Summer <1106619871@qq.com>
2020-02-13 16:16:16 +0800

    feat: init blog's frontend part

---
+++ blog/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+import { Button } from 'antd'
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <div><Button>测试</Button></div>
+ </>
+)
+
+export default Home
